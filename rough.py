e576da68c7b1b77a9e8dd0c320b7166ce30580cc
JatinTekam <tekamjatin@gmail.com>
2026-01-15 20:50:26 +0530

    modify llm_client.py file

--- rough.py
+++ rough.py
@@ -1,6 +1,16 @@
-from config.model_config import load_config
+# from config.model_config import load_config
 
-cfg = load_config()
+# cfg = load_config()
 
-print(cfg.model);
-print(cfg.debugging_app);
+# print(cfg.model);
+# print(cfg.debugging_app);
+
+# -----------------------------------------
+
+# test-> llm_client.py
+
+from utils.llm_client import GeminiClient
+
+client = GeminiClient()
+
+print(client.ask("What is capital of newzland"))
